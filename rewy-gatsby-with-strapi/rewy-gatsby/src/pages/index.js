a2b13082a090aebeb433460b545055e703bc156d
Christopher <toholderandrews@gmail.com>
2021-03-15 14:32:53 -0600

    removed import of no used components

--- rewy-gatsby-with-strapi/rewy-gatsby/src/pages/index.js
+++ rewy-gatsby-with-strapi/rewy-gatsby/src/pages/index.js
@@ -5,11 +5,7 @@ import Layout from "../components/App/Layout"
 import Banner from "../components/Index/Banner"
 import OurSolutions from "../components/Index/OurSolutions"
 import OurServices from "../components/Index/OurServices"
-import OurFeatures from "../components/Index/OurFeatures"
-import TeamMember from "../components/Index/TeamMember"
 import RecentProjects from "../components/Index/RecentProjects"
-import Pricing from "../components/Index/Pricing"
-import Testimonials from "../components/Index/Testimonials"
 import Partner from "../components/Index/Partner"
 import ProjectStartArea from "../components/Index/ProjectStartArea"
 import OurBlog from "../components/Index/OurBlog"
@@ -21,17 +17,7 @@ const Home = () => {
       <Banner />
       <OurSolutions />
       <OurServices />
-      {/*
-      <OurFeatures />
-      */} 
-      {/*
-      <TeamMember />
-      */}
       <RecentProjects />
-      {/* 
-      <Pricing />
-      <Testimonials />
-      */}
       <Partner />
       <OurBlog />
       <ProjectStartArea />
