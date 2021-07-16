9fb1d6213fed83bb6d77af5eb62ceafc46a1281f
Christopher <toholderandrews@gmail.com>
2021-07-16 20:00:26 +0200

    refactor: improve style of index page

--- deepblue-gatsby/src/components/Index/Banner.js
+++ deepblue-gatsby/src/components/Index/Banner.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import ReactWOW from 'react-wow'
 
 import { Link } from 'gatsby'
 import { getImage, StaticImage } from 'gatsby-plugin-image'
@@ -37,35 +36,27 @@ const Banner = () => {
                 <div className="row align-items-center">
                     <div className="col-lg-6 col-md-12">
                         <div className="main-banner-content">
-                            <ReactWOW delay='.1s' animation='fadeInLeft'>
-                                <h1>Grow your business with custom software</h1>
-                            </ReactWOW>
+                            <h1>Grow your business with custom software</h1>
 
-                            <ReactWOW delay='.1s' animation='fadeInLeft'>
-                                <p>Our goal at Deep Blue is to help our customers grow and join the digitalization revolution.</p>
-                            </ReactWOW>
+                            <p>Our goal at Deep Blue is to help our customers grow and join the digitalization revolution.</p>
 
-                            <ReactWOW delay='.1s' animation='fadeInRight'>
-                                <div className="btn-box">
-                                    <Link to="/contact" className="default-btn">
-                                        <i className="flaticon-right"></i> 
-                                        Get Started <span></span>
-                                    </Link>
-                                </div>
-                            </ReactWOW>
+                            <div className="btn-box">
+                                <Link to="/contact" className="default-btn">
+                                    <i className="flaticon-right"></i> 
+                                    Get Started <span></span>
+                                </Link>
+                            </div>
                         </div>
                     </div>
 
                     <div className="col-lg-6 col-md-12">
-                        <ReactWOW delay='.1s' animation='fadeInUp'>
-                            <div className="main-banner-image">
-                                <StaticImage
-                                    src="../../assets/images/banner-img1.png"
-                                    alt="animation of two woman and two men doing digital work"
-                                    placeholder="tracedSVG"
-                                />
-                            </div>
-                        </ReactWOW>
+                        <div className="main-banner-image">
+                            <StaticImage
+                                src="../../assets/images/banner-img1.png"
+                                alt="animation of two woman and two men doing digital work"
+                                placeholder="tracedSVG"
+                            />
+                        </div>
                     </div>
                 </div>
             </div>
