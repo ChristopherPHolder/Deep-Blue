902acea9eaa7d4199a1994d775356ba8f528c2fb
Christopher <toholderandrews@gmail.com>
2021-07-31 15:57:40 +0200

    refactor: move blog into sub url in new folder

---
+++ deepblue-gatsby/src/pages/blog-details/Virtualization-and-Cloud-Computing.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Layout from "../../components/App/Layout"
+import PageBanner from '../../components/Common/PageBanner'
+import BlogDetailsContent from '../../components/BlogContent/BlogDetailsContent'
+
+const BlogDetails = () => {
+    return (
+        <Layout 
+        title="Virtualization and Cloud Computing"
+        description="An in-depth introduction to client-side and 
+        server-side virtualization, types of hypervisors, 
+        cloud computing, and its 3 common models SaaS, PaaS, and IaaS."
+        type="article" 
+        url="https://www.deep-blue.io//blog-details/Virtualization-and-Cloud-Computing/" 
+        imageUrl="https://deep-blue.io/static/893521fad2ade0f0c2e2b1ec751f5630/7a708/cloud-computing.webp"
+        >
+            <PageBanner
+                pageTitle="Virtualization and Cloud computing" 
+                homePageText="Home" 
+                homePageUrl="/" 
+                activePageText="Blog Details" 
+            /> 
+            <BlogDetailsContent />
+        </Layout>
+    )
+}
+
+export default BlogDetails;
