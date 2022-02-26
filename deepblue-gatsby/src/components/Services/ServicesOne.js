4e3b0b8763f2a4be6320c4c78559876945bc2615
Christopher Holder <toholderandrews@gmail.com>
2022-02-26 16:09:31 +0100

    refactor: improve code design by ectracting data and using map funtion

--- deepblue-gatsby/src/components/Services/ServicesOne.js
+++ deepblue-gatsby/src/components/Services/ServicesOne.js
@@ -1,57 +1,42 @@
-import React from 'react'
+import React from 'react';
+
+const services = [
+    {
+        serviceName: 'Web Applications',
+        serviceDescription: 'Web application and development support. We can build it with you from start to finish or come support you when you need help.',
+        serviceFavicon: 'flaticon-rocket'
+    },
+    {
+        serviceName: 'Digital Automation',
+        serviceDescription: 'We do not like soul-breaking repetitive work, but our software really does. It needs no breaks, does not make mistakes, and takes no salary.',
+        serviceFavicon: 'flaticon-laptop'
+    },
+    {
+        serviceName: 'Digitalization Consulting',
+        serviceDescription: 'Inefficiencies waste time and hurt morale, and more often than not you do not need more people, you need better processes and better software.',
+        serviceFavicon: 'flaticon-money'
+    }
+]
+
 
 const ServicesOne = () => {
     return (
         <section className="solutions-area pt-100 pb-70">
             <div className="container">
                 <div className="row">
-                    <div className="col-lg-4 col-md-6 col-sm-6">
-                        <div className="single-solutions-box">
-                            <div className="icon">
-                                <i className="flaticon-rocket"></i>
+                    {services.map((service) =>(
+                        <div className="col-lg-4 col-md-6 col-sm-6">
+                            <div className="single-solutions-box">
+                                <div className="icon"><i className={service.serviceFavicon}></i></div>
+                                <h3>{service.serviceName}</h3>
+                                <p>{service.serviceDescription}</p>
                             </div>
-                            <h3>
-                                Web Applications
-                            </h3>
-
-                            <p>Web application and development support. We can build it with you from start to finish or come support you when you need help.</p>
-
                         </div>
-                    </div>
-
-                    <div className="col-lg-4 col-md-6 col-sm-6">
-                        <div className="single-solutions-box">
-                            <div className="icon">
-                                <i className="flaticon-laptop"></i>
-                            </div>
-   
-                            <h3>
-                                Digital Automation
-                            </h3>
-
-                            <p>We don't like soul-breaking repetitive work, but our software really does. It needs no breaks, doesn't make mistakes, and takes no salary.</p>
-                            
-                        </div>
-                    </div>
-
-                    <div className="col-lg-4 col-md-6 col-sm-6">
-                        <div className="single-solutions-box">
-                            <div className="icon">
-                                <i className="flaticon-money"></i>
-                            </div>
-
-                            <h3>
-                                Digitalization Consulting
-                            </h3>
-
-                            <p>Inefficiencies waste time and hurt morale, and more often than not you don't need more people, you need better processes and better software.</p>
-                            
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </div>
         </section>
     )
 }
 
-export default ServicesOne
+export default ServicesOne;
