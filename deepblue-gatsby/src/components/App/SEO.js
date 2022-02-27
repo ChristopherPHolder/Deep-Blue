0218e5a861c2e392ce9c741bf0364a8095976c84
Christopher Holder <toholderandrews@gmail.com>
2022-02-27 21:01:42 +0100

    feat: add meta card generation to seo properties

--- deepblue-gatsby/src/components/App/SEO.js
+++ deepblue-gatsby/src/components/App/SEO.js
@@ -15,7 +15,14 @@ const Seo = ({ title, description, type, url, imageUrl }) => {
                 <meta name="og:description" property="og:description" content={description} />
                 <meta name="og:url" property="og:url" content={url} />
                 <meta name="og:image" property="og:image" content={fullImageUrl} />
-                <meta name="twitter:card" content="Deep Blue - Custom software to speed up your business"/>
+
+                <meta name="twitter:card" property="twitter:card" content="summary"/>
+                <meta name="twitter:title"  property="twitter:title" content={title_name} />
+                <meta name="twitter:description" property="twitter:description" content={description} />
+                <meta name="twitter:site" property="twitter:site" content="@DeepBlue_io" />
+                <meta name="twitter:image" property="twitter:image" content={fullImageUrl} />
+                <meta name="twitter:label1" property="twitter:label1" content="Reading time" />
+                <meta name="twitter:data1" property="twitterdata1" content="5 min read" />
             </Helmet>
         </div>
     )
