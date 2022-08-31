749f502f22580b94213796655ae99a9b939e489b
Chris <chris@deep-blue.io>
2022-08-31 13:13:21 +0200

    fix: add first userflow

--- deepblue-lambda/user-flows/user-flows/app.ts
+++ deepblue-lambda/user-flows/user-flows/app.ts
@@ -1,7 +1,18 @@
-export const lambdaHandler = async (targetUrl: URL): Promise< string| Error > => {
-    
+import  {launch}  from "puppeteer";
+
+// @ts-ignore
+import {UserFlow} from 'lighthouse/lighthouse-core/fraggle-rock/user-flow';
+import FlowResult from 'lighthouse/types/lhr/flow';
+
+export const lambdaHandler = async (targetUrl: URL): Promise<FlowResult | Error> => {
     try {
-        return '';
+        const browser = await launch({headless: true});
+        const page = await browser.newPage();
+        const flow: UserFlow = await new UserFlow(page);
+        await flow.navigate(targetUrl.href);
+        const report = await flow.createFlowResult();
+        await browser.close();
+        return report;
     } catch (err) {
         console.log(err);
         throw(err);
