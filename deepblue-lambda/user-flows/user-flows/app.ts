087ad7fed86be1db3f3644415627296521659408
Chris <chris@deep-blue.io>
2022-08-31 14:06:56 +0200

    fix: handle target url as event param

--- deepblue-lambda/user-flows/user-flows/app.ts
+++ deepblue-lambda/user-flows/user-flows/app.ts
@@ -4,12 +4,26 @@ import  {launch}  from "puppeteer";
 import {UserFlow} from 'lighthouse/lighthouse-core/fraggle-rock/user-flow';
 import FlowResult from 'lighthouse/types/lhr/flow';
 
-export const lambdaHandler = async (targetUrl: URL): Promise<FlowResult | Error> => {
+interface Event {
+    targetUrl: string
+}
+
+export const lambdaHandler = async (event: Event): Promise<FlowResult|String> => {
+    const { targetUrl } = event;
+    let url: URL;
+    try {
+        url = new URL(targetUrl);
+    } catch {
+        const error = `ERROR: invalid url -> ${targetUrl}`
+        console.log(error)
+        return error;
+    }
+    
     try {
         const browser = await launch({headless: true});
         const page = await browser.newPage();
         const flow: UserFlow = await new UserFlow(page);
-        await flow.navigate(targetUrl.href);
+        await flow.navigate(url.href);
         const report = await flow.createFlowResult();
         await browser.close();
         return report;
--- deepblue-lambda/user-flows/user-flows/tests/unit/test-handler.test.ts
+++ deepblue-lambda/user-flows/user-flows/tests/unit/test-handler.test.ts
@@ -6,6 +6,7 @@ import { join } from 'path';
 import { readdir, stat } from 'fs/promises';
 
 const TEST_TARGET_URL_STRING = 'https://deep-blue.io/'
+const TEST_INVALID_TARGET_URL_STRING = 'invalid_url_test_string';
 
 describe('Unit test for app handler', function () {
 
@@ -28,20 +29,32 @@ describe('Unit test for app handler', function () {
         const size = await dirSize('../.aws-sam');
 
         expect(size).toBeTruthy();
-        expect(size).toBeLessThanOrEqual(1197984);
+        expect(size).toBeLessThanOrEqual(1198088);
 
     });
 
-    it('should run a user flow test and return report', async () => {
+    it('should fail with an invalid url as a target url', async () => {
+        const event = {
+            targetUrl: TEST_INVALID_TARGET_URL_STRING
+        }
 
-        const targetUrl = new URL(TEST_TARGET_URL_STRING);
-        const result = await lambdaHandler(targetUrl) as FlowResult;
+        const result = await lambdaHandler(event) as FlowResult;
+
+        expect(result).toEqual(`ERROR: invalid url -> ${event.targetUrl}`);
+    }, 100);
+
+    it('should run a user flow test and return report', async () => {
+        const event = {
+            targetUrl: TEST_TARGET_URL_STRING
+        }
+
+        const result = await lambdaHandler(event) as FlowResult;
 
         expect(result).toBeTruthy();
         expect(typeof result).toEqual("object");
         expect(result.name).toContain("User flow");
-        expect(result.name).toContain(targetUrl.hostname);
 
+        const url = new URL(event.targetUrl); 
+        expect(result.name).toContain(url.hostname);
     }, 20000);
-
 });
