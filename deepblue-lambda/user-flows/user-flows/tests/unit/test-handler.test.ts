479d7819318226430d9924444dd73239d11eb14e
Chris <chris@deep-blue.io>
2022-08-31 13:16:45 +0200

    fix: add test to insure userflow ran sucessfully

--- deepblue-lambda/user-flows/user-flows/tests/unit/test-handler.test.ts
+++ deepblue-lambda/user-flows/user-flows/tests/unit/test-handler.test.ts
@@ -1,18 +1,22 @@
 import { lambdaHandler } from '../../app';
 
+import FlowResult from 'lighthouse/types/lhr/flow';
+
 const TEST_TARGET_URL_STRING = 'https://deep-blue.io/'
 
 describe('Unit test for app handler', function () {
 
-    it('verifies successful response', async () => {
-        
+
+    it('should run a user flow test and return report', async () => {
+
         const targetUrl = new URL(TEST_TARGET_URL_STRING);
+        const result = await lambdaHandler(targetUrl) as FlowResult;
 
-        const result = await lambdaHandler(targetUrl);
+        expect(result).toBeTruthy();
+        expect(typeof result).toEqual("object");
+        expect(result.name).toContain("User flow");
+        expect(result.name).toContain(targetUrl.hostname);
 
-        expect(typeof result).toEqual('string');
+    }, 20000);
 
-        // expect(result).toContain(TEST_TARGET_URL_STRING);
-
-    });
 });
